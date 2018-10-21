694dd5ea4f333c2983c40abc8ea8b7ed43381ad5
Rashwan <abdo.ra.1997@gmail.com>
2018-10-21 11:10:38 +0200

    changed interface to abstract and edited tests to assert json

--- src/Layouts/Layout.php
+++ src/Layouts/Layout.php
@@ -9,46 +9,94 @@
 namespace EquineSolutions\IOCFilemaker\Layouts;
 
 
-interface Layout
+use EquineSolutions\IOCFilemaker\Connector;
+
+abstract class Layout
 {
     /**
      * returns the name of the layout
      * @return string
      */
-    public function getLayout();
+    public abstract function getLayout();
 
     /**
      * returns list of the resource
      * @return array
+     * @throws \airmoi\FileMaker\FileMakerException
      */
-    public function index();
+    public function index()
+    {
+        $filemaker = (new Connector())->filemaker();
+        return translateALl($filemaker->newFindCommand($this->getLayout())
+            ->execute()
+            ->getRecords(), $filemaker);
+    }
 
     /**
      * returns specified resource
      * @param $id
      * @return array
+     * @throws \airmoi\FileMaker\FileMakerException
      */
-    public function show($id);
+    public function show($id)
+    {
+        $filemaker = (new Connector())->filemaker();
+        return translateALl($filemaker->newFindCommand($this->getLayout())
+            ->addFindCriterion('show_id', '='.$id)
+            ->execute()
+            ->getRecords(), $filemaker);
+    }
 
     /**
      * creates new resources with given data
      * @param $data
      * @return array
+     * @throws \airmoi\FileMaker\FileMakerException
+     * @throws \airmoi\FileMaker\FileMakerValidationException
      */
-    public function create($data);
+    public function create($data)
+    {
+        $filemaker = (new Connector())->filemaker();
+        return translateALl($filemaker->newAddCommand($this->getLayout(), $data)
+            ->execute()
+            ->getRecords(), $filemaker);
+    }
 
     /**
      * updates resource with record id specified
      * @param $record_id
      * @param $data
      * @return array
+     * @throws \airmoi\FileMaker\FileMakerException
+     * @throws \airmoi\FileMaker\FileMakerValidationException
      */
-    public function update($record_id, $data);
+    public function update($record_id, $data)
+    {
+        $filemaker = (new Connector())->filemaker();
+        return translateALl($filemaker->newEditCommand($this->getLayout(), $record_id, $data)
+            ->execute()
+            ->getRecords(), $filemaker);
+    }
 
     /**
      * deletes resource with record id specified
      * @param $record_id
      * @return array
+     * @throws \airmoi\FileMaker\FileMakerException
      */
-    public function delete($record_id);
+    public function delete($record_id)
+    {
+        $filemaker = (new Connector())->filemaker();
+        return translateALl($filemaker->newDeleteCommand($this->getLayout(), $record_id)
+            ->execute()
+            ->getRecords(), $filemaker);
+    }
 }
+
+
+
+
+
+
+
+
--- tests/ShowsLayoutTest.php
+++ tests/ShowsLayoutTest.php
@@ -18,7 +18,7 @@ class ShowsLayoutTest extends TestCase
         $results = $showsLayout->index();
         // Then I should see
 //        $this->assertTrue(true);
-        $this->assertArrayHasKey(0, $results);
+        $this->assertJson(json_encode($results));
     }
 
     /** @test */
@@ -26,39 +26,9 @@ class ShowsLayoutTest extends TestCase
     {
         $showsLayout = new ShowsLayout();
 
-        $result = $showsLayout->show(1);
+        $results = $showsLayout->show(1);
 
-        $this->assertArraySubset($result,
-            Array(
-                0 => Array(
-                    'show_id' => '1',
-                    'Show Name' => 'The Gulf Elite Tour',
-                    'Date From' => '01/25/2018',
-                    'Date To' => '01/27/2018',
-                    'Year' => '2018',
-                    'Location' => 'Sharjah Equestrain Club',
-                    'Country' => 'UAE',
-                    'Organizer Name' => 'Shk. Ali Al Qassmi',
-                    'Organizer Contact' => '',
-                    'Organizer Address' => 'Sharjah Club',
-                    'Show Prize Money' => '',
-                    'Number Of Events' => '1',
-                    'Horse Per Show' => '3',
-                    'LOGO' => 'http://n411.fmphost.com/fmi/xml/cnt/AAAAA_logo_.jpg?-db=IOCT&-lay=PHP_SHOW&-recid=75&-field=LOGO(1)',
-                    'ksponsor1logo' => '',
-                    'ksponsor2logo' => '',
-                    'ksponsor3logo' => '',
-                    'ksponsor4logo' => '',
-                    'ksponsor5logo' => '',
-                    'ksponsor6logo' => '',
-                    'Currency' => 'AED',
-                    '' => '',
-                    'modification timestamp' => '10/12/2018 16:47:13',
-                    'FEI_Prog' => 'https://data.fei.org/Calendar/DraftSchedulePush.aspx?p=866188174EE9B18C2858B0D6054573BC6CE4C4A1E875EDAFD51DF5E8777D7D150A61709D0F8E804F8D1AB4B9C50F600132F53EA5C44ADCF2B176F5ECD53D9D90010A5A85C18F284DAF1585FCFDB4E6E27A0B45F98D78E3DFCAB1989CC4FFD6AD8A6AABF27E1F9CD29706158DAAB7D7D98D62284FD90A343D6CE6083EED4D40E4C6FF8DA680ABC0A99105D90E3E3095CE4E1354866FC45C613689C901437818A3',
-                    'record_id' => '75',
-                )
-            )
-         );
+        $this->assertJson(json_encode($results));
     }
 
     /** @test */
@@ -66,12 +36,12 @@ class ShowsLayoutTest extends TestCase
     {
         $showLayout = new ShowsLayout();
 
-        $result = $showLayout->create([
+        $results = $showLayout->create([
             'show_id' => '1001',
             'Show Name' => 'test show'
         ]);
 
-        $this->assertArrayHasKey(0, $result);
+        $this->assertJson(json_encode($results));
     }
 
     /** @test */
@@ -81,11 +51,11 @@ class ShowsLayoutTest extends TestCase
         $indexResponse = $showLayout->index();
         $last_record_id = $indexResponse[sizeof($indexResponse)-1]['record_id'];
 
-        $result = $showLayout->update($last_record_id, [
+        $results = $showLayout->update($last_record_id, [
             'Show Name' => 'test show 2'
         ]);
 
-        $this->assertArrayHasKey(0, $result);
+        $this->assertJson(json_encode($results));
     }
 
     /** @test */
@@ -94,9 +64,9 @@ class ShowsLayoutTest extends TestCase
         $showLayout = new ShowsLayout();
         $indexResponse = $showLayout->index();
         $last_record_id = $indexResponse[sizeof($indexResponse)-1]['record_id'];
-        $result = $showLayout->delete($last_record_id);
+        $results = $showLayout->delete($last_record_id);
 
-        $this->assertArraySubset($result, []);
+        $this->assertJson(json_encode($results));
     }
 }
 
