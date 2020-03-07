77cc2c7e9f15afc5c7743021f4394c8240b1ceca
unknown <abdo.ra.1997@gmail.com>
2020-03-07 14:47:29 +0200

    removed config

--- config/filemaker.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'default-database' => env('FILEMAKER_DEFAULT_DATABASE'),
-    'hostspec' => env('FILEMAKER_HOSTSPEC'),
-    'username' => env('FILEMAKER_USERNAME'),
-    'password' => env('FILEMAKER_PASSWORD'),
-];
