ccd8b4d1a1719798021207dbcddaa9df86d614fe
unknown <abdo.ra.1997@gmail.com>
2020-03-21 18:20:09 +0200

    removed unneccessary code

--- config/filemaker.php
+++ config/filemaker.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => env('FILEMAKER_HOSTSPEC', 'n471.fmphost.com'),
-    'username' => env('FILEMAKER_USERNAME', 'Admin'),
-    'password' => env('FILEMAKER_PASSWORD', 'P@ssw0rd9666'),
+    'host' => env('FILEMAKER_HOSTSPEC'),
+    'username' => env('FILEMAKER_USERNAME'),
+    'password' => env('FILEMAKER_PASSWORD'),
 ];
